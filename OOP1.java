ae28a759f5d5c21913ed08fb16540271a6ac7562
Saidilhalim <125287379+Saidilhalim@users.noreply.github.com>
2023-02-13 22:25:45 +0700

    Add files via upload

---
+++ OOP1.java
@@ -0,0 +1,65 @@
+public class OOP1 {
+    public static void main(String[] args) {
+        //        Deklarasi variable
+//        int a,b,c;
+//        a = 4;
+//        b = 6;
+//        c = a+b;
+//        System.out.println("hasilnya" +c );
+//
+//        int var1;
+//        int var2;
+//        float var3;
+//
+//        var1 = 1024;
+//        var2 = var1/3;
+//        var3 = (float)var1/3;
+//
+//        System.out.println("var1 : " + var1);
+//        System.out.println("var2 : " + var2);
+//        System.out.println("var3 : " + var3);
+//
+////        volume balok
+//        int p,l,t,v;
+//        p = 58;
+//        l = 88;
+//        t = 7;
+//        v = p*l*t;
+//        System.out.println("hasilnya : " + v);
+//
+////        luas segitiga
+//        int pa,tt,vs;
+//        pa = 98;
+//        tt = 57;
+//        vs =  (pa * tt) * 1/2;
+//        System.out.println("hasilnya : " + vs);
+//
+////        galon ke litter
+//        float gallon = 3.7854f;
+//        int jumlah = 10;
+//        float litter = gallon * jumlah;
+//        System.out.println(jumlah + " hasillnya " + litter);
+//
+////        percabbangan 1
+//        int x = 50;
+//        int y = 10;
+//        if(x>y){
+//            System.out.println("hello world");
+//        }
+//
+////        latihan switch case
+//        int hari;
+//        hari =10;
+//        switch (hari){
+//            case 1 : System.out.println("senin"); break;
+//            case 2 : System.out.println("selasa"); break;
+//            case 3 : System.out.println("rabu"); break;
+//            case 4 : System.out.println("kamis"); break;
+//            case 5 : System.out.println("jumat"); break;
+//            case 6 : System.out.println("sabtu"); break;
+//            case 7 : System.out.println("minggu"); break;
+//            default:  System.out.println("tidak ada");
+//
+//        }
+    }
+}
